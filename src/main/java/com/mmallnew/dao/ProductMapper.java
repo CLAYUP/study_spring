7fc925c1b2eafaa449f075b2e6c9c161ec74d0d0
yang <1021043755@qq.com>
2019-02-07 12:57:01 +0800

    project init commit

---
+++ src/main/java/com/mmallnew/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.mmallnew.dao;
+
+import com.mmallnew.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
